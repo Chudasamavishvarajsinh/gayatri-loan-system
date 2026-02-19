9bc2768a4974ffa2abb41b2bfc7f55e3b470d433
Chudasamavishvarajsinh <143185034+Chudasamavishvarajsinh@users.noreply.github.com>
2026-02-19 23:41:14 +0530

    Update history.js

--- js/history.js
+++ js/history.js
@@ -2,6 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore, collection, query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDg536DdCRGBTacV4wx1_asQ6NyOflX01I",
   authDomain: "gayatri-loan-system.firebaseapp.com",
@@ -10,6 +16,9 @@ const firebaseConfig = {
   messagingSenderId: "52394591148",
   appId: "1:52394591148:web:11e859e4d0f411ec4af009"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
