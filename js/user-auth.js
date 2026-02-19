9f55b9875ae5d66f72834a3e2d7ebea703f79cc2
Chudasamavishvarajsinh <143185034+Chudasamavishvarajsinh@users.noreply.github.com>
2026-02-19 23:41:39 +0530

    Update user-auth.js

--- js/user-auth.js
+++ js/user-auth.js
@@ -1,6 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDg536DdCRGBTacV4wx1_asQ6NyOflX01I",
   authDomain: "gayatri-loan-system.firebaseapp.com",
@@ -10,6 +16,8 @@ const firebaseConfig = {
   appId: "1:52394591148:web:11e859e4d0f411ec4af009"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
