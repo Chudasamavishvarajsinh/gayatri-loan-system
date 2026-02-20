9bc9077f0deb1e0bfd59b7ce7f1cd06f3357f807
Chudasamavishvarajsinh <143185034+Chudasamavishvarajsinh@users.noreply.github.com>
2026-02-20 21:22:22 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,3 @@
-// js/firebase-config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -8,14 +6,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyDg536DdCRGBTacV4wx1_asQ6NyOflX01I",
   authDomain: "gayatri-loan-system.firebaseapp.com",
   projectId: "gayatri-loan-system",
-  storageBucket: "gayatri-loan-system.firebasestorage.app",
+  storageBucket: "gayatri-loan-system.appspot.com",
   messagingSenderId: "52394591148",
   appId: "1:52394591148:web:11e859e4d0f411ec4af009"
 };
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
