4f083fc18c2ee191aaefced4aadd127ba5659fce
Chudasamavishvarajsinh <143185034+Chudasamavishvarajsinh@users.noreply.github.com>
2026-02-22 00:08:02 +0530

    Update user-login.js

--- js/user-login.js
+++ js/user-login.js
@@ -1,3 +1,4 @@
+console.log("User login JS loaded");
 import { auth } from "./firebase-config.js";
 import { signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
